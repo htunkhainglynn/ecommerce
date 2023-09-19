da4b7c7aa32ca66fb8959e206e304bc3905f643d
Htun Khaing Lynn <htunkhainglynn13@gmail.com>
2023-09-20 01:04:40 +0630

    update RabbitMQ config

--- src/main/java/com/project/ecommerce/config/RabbitMQConfig.java
+++ src/main/java/com/project/ecommerce/config/RabbitMQConfig.java
@@ -1,6 +1,8 @@
 package com.project.ecommerce.config;
 
-import org.springframework.amqp.core.*;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
@@ -28,21 +30,9 @@ public class RabbitMQConfig {
     private String virtualHost;
 
     @Bean
-    public Queue topicQueue() {
-        return new Queue("ecommerce.topic.queue", true, false, false);
-    }
-
-    @Bean
-    public Queue fanoutQueue() {
-        return new Queue("ecommerce.fanout.queue", true, false, false);
-    }
-
-    @Bean
-    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory, TopicExchange topicExchange, FanoutExchange fanoutExchange) {
-        RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
-        rabbitAdmin.declareBinding(BindingBuilder.bind(topicQueue()).to(topicExchange).with("ecommerce.topic.key.*"));
-        rabbitAdmin.declareBinding(BindingBuilder.bind(fanoutQueue()).to(fanoutExchange));
-        return rabbitAdmin;
+    public DirectExchange directExchange() {
+        String directExchangeName = "ecommerce-direct-exchange";
+        return new DirectExchange(directExchangeName);
     }
 
     @Bean
@@ -56,23 +46,9 @@ public class RabbitMQConfig {
         return connectionFactory;
     }
 
-
     @Bean
-    public DirectExchange directExchange() {
-        String directExchangeName = "ecommerce-direct-exchange";
-        return new DirectExchange(directExchangeName);
-    }
-
-    @Bean
-    public FanoutExchange fanoutExchange() {
-        String fanoutExchangeName = "ecommerce-fanout-exchange";
-        return new FanoutExchange(fanoutExchangeName);
-    }
-
-    @Bean
-    public TopicExchange topicExchange() {
-        String topicExchangeName = "ecommerce-topic-exchange";
-        return new TopicExchange(topicExchangeName);
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
     }
 
     @Bean
