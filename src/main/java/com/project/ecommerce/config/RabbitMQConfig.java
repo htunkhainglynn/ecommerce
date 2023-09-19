6617c5708727d542f636f9b2c8ea8afc3570de54
Htun Khaing Lynn <htunkhainglynn13@gmail.com>
2023-09-19 23:34:32 +0630

    change rabbitMQ port

--- src/main/java/com/project/ecommerce/config/RabbitMQConfig.java
+++ src/main/java/com/project/ecommerce/config/RabbitMQConfig.java
@@ -5,11 +5,27 @@ import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
+
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    @Value("${spring.rabbitmq.port}")
+    private int port;
+
+    @Value("${spring.rabbitmq.virtual-host}")
+    private String virtualHost;
 
     @Bean
     public Queue topicQueue() {
@@ -31,8 +47,15 @@ public class RabbitMQConfig {
 
     @Bean
     public CachingConnectionFactory connectionFactory() {
-        return new CachingConnectionFactory("localhost");
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setHost(host);
+        connectionFactory.setUsername(username);
+        connectionFactory.setPassword(password);
+        connectionFactory.setPort(port);
+        connectionFactory.setVirtualHost(virtualHost);
+        return connectionFactory;
     }
+
 
     @Bean
     public DirectExchange directExchange() {
